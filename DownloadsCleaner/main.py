b0d80ce15c050511b18660b489890caf95ca65b9
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-05-30 15:12:05 +0545

    add main code

--- DownloadsCleaner/main.py
+++ DownloadsCleaner/main.py
@@ -1,48 +1,39 @@
-# This program automatically sorts the items in your downloads directory and puts them in appropriate directories
-
 import os 
 import shutil
 
 def main():
-    current_dir = os.getcwd()
-    
-    source_dir = "test"
-    dest_dir = "sortedData"
+    # Source and destination paths
+    source_path = r"C:\Users\adhik\Downloads"
+    dest_path = r"C:\Users\adhik\Downloads\SortedItems"
 
-    # get paths of the directories
-    source_path = os.path.join(current_dir, source_dir)
-    dest_path = os.path.join(current_dir, dest_dir)
-
-    # create directories if they don't already exist
-    if not os.path.exists(source_path):
-        os.makedirs(source_dir)
-
+    # Create the destination directory if it doesn't exist
     if not os.path.exists(dest_path):
-        os.makedirs(dest_dir)
+        os.makedirs(dest_path)
 
-    # create directories to store files
+    # Create directories to store files
     new_dirs = ['images', 'documents', 'pdfs', 'others']
-    for dir in new_dirs:
-        dir_path = os.path.join(dest_path, dir)
+    for dir_name in new_dirs:
+        dir_path = os.path.join(dest_path, dir_name)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-
+    # Iterate over files in the source directory
     for file in os.listdir(source_path):
-        if file.split(".")[1] in ['jpg', 'jpeg', 'png', 'gif']:
-            shutil.move(os.path.join(source_dir, file), os.path.join(dest_path, "images"))
+        file_path = os.path.join(source_path, file)
+        # Check the file extension and move it to the appropriate directory
+        if os.path.isfile(file_path):
+            file_extension = file.split(".")[-1].lower()
+            if file_extension in ['jpg', 'jpeg', 'png', 'gif']:
+                shutil.move(file_path, os.path.join(dest_path, "images", file))
+            elif file_extension in ['txt', 'docx']:
+                shutil.move(file_path, os.path.join(dest_path, "documents", file))
+            elif file_extension in ['pdf']:
+                shutil.move(file_path, os.path.join(dest_path, "pdfs", file))
+            else:
+                shutil.move(file_path, os.path.join(dest_path, "others", file))
+            print(f"Moved {file} to the appropriate directory.")
 
-        elif file.split(".")[1] in ['txt', 'docx',]:
-            shutil.move(os.path.join(source_dir, file), os.path.join(dest_path, "documents"))
+    print("Files sorted successfully.")
 
-        elif file.split(".")[1] in ['pdf']:
-            shutil.move(os.path.join(source_dir, file), os.path.join(dest_path, "pdfs"))
-
-        else:
-            shutil.move(os.path.join(source_dir, file), os.path.join(dest_dir, "others"))
-
-    print("Files moved successfully")
-
-    
 if __name__ == '__main__':
     main()
