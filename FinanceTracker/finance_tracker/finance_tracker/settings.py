0bc709649ae65ac371046362baf0e53a4ee82e1b
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-06-09 11:23:26 +0545

    save changes before filter-branch operation

--- FinanceTracker/finance_tracker/finance_tracker/settings.py
+++ FinanceTracker/finance_tracker/finance_tracker/settings.py
@@ -138,3 +138,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MESSAGE TAG CHANGE
 MESSAGE_TAGS = {message_constants.ERROR: "danger"}
+
+# EMAIL 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
