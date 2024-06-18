f3f78253d42ff76ba911060d3e036c3f4d48acd8
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-06-18 13:39:53 +0545

    add pagination to expenses table

--- FinanceTracker/finance_tracker/expenses/views.py
+++ FinanceTracker/finance_tracker/expenses/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 
 @login_required(login_url='/auth/login')
 def index(request):
+    pageItemCount = 2
     expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, pageItemCount)
+    page_num = int(request.GET.get('page'))
+    page_obj = Paginator.get_page(paginator, page_num)
+    page_obj = paginator.get_page(page_num)
+    
+    # Calculate the range of pages to display
+    index = page_num - 1  # Current page index
+    max_index = paginator.num_pages
+    start_index = index - 1 if index >= 1 else index
+    end_index = index + 2 if index <= max_index - 3 else max_index
+    # Generate the page range to display
+    page_range = paginator.page_range[start_index:end_index]
     context = {
-        'expenses': expenses
+        'expenses': expenses,
+        'page_obj': page_obj,
+        'page_range': page_range
     }
     return render(request, 'expenses/index.html', context)
 
