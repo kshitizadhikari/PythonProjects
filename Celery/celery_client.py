ffc309203b00c4997ee088474bda11eb750cbc91
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-06-24 13:27:10 +0545

    remove old project folders

--- Celery/celery_client.py
+++
@@ -1,7 +0,0 @@
-from tasks import add
-
-# Send the task to the Celery worker
-result = add.delay(4, 2)
-
-# Optionally, wait for the result
-# print(result.get(timeout=10))  # Wait for the result and print it
--- jwt-practice/init_db.py
+++
@@ -1,6 +0,0 @@
-from database.db import Base, engine
-from models.models import User
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables created successfully")
